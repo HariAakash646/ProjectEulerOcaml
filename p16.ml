d1d24ca31d25cd14c154c7266279b4fd62b9a0d1
HariAakash646 <hariaakash646@gmail.com>
2025-12-29 05:23:10 +0530

    Solved problem 16

---
+++ p16.ml
@@ -0,0 +1,10 @@
+let x = Z.of_int 2
+let p = Z.of_int 1000
+
+let num =
+  List.map
+    (fun x -> int_of_char x - int_of_char '0')
+    (List.of_seq (String.to_seq (Z.to_string (Z.pow x 1000))))
+;;
+
+print_int (List.fold_left (fun acc x -> acc + x) 0 num)
